d911cda42e5909080f80447b184786948fc47efe
Samyak Jain <jsamyak799@gmail.com>
2025-07-24 18:32:00 +0530

    Application and CI/CD Pipeline

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+import pickle
 import os
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -63,5 +64,12 @@ def model_evaluate(X_train,Y_train,X_test,Y_test,models,param):
             }
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
